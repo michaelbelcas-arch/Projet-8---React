3ff21f25a0a385299f886469a0af0a2dfda1672f
Michael Belcastro <michaelbelcas@gmail.com>
2026-04-13 09:39:02 +0200

    Pages simples et démarrage.

---
+++ projet-react-kasa/src/pages/about/About.jsx
@@ -0,0 +1,11 @@
+import './about.css'
+
+function About() {
+  return (
+    <main className="about">
+      <h1>À propos</h1>
+    </main>
+  )
+}
+
+export default About
---
+++ projet-react-kasa/src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import './home.css'
+
+function Home() {
+  return (
+    <main className="home">
+      <h1>Page d'accueil</h1>
+    </main>
+  )
+}
+
+export default Home
